c93cc9198c946228da67ac222bd37cc59646fab7
rnwxyz <jamesstrom78@gmail.com>
2022-05-26 01:48:57 +0800

    fix redirect dashboard, login and regist form.

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,30 +1,58 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
+import Surveys from "../views/Surveys.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
+import DefaultLayout from "../components/DefaultLayout.vue"
+import AuthLayout from "../components/AuthLayout.vue"
+import store from "../store/index"
 
 
 const routes = [
   {
     path: '/',
-    name: 'Dashboard',
-    component: Dashboard
+    redirect: '/dashboard',
+    component: DefaultLayout,
+    meta: {requiresAuth: true},
+    children: [
+      {path: '/dashboard', name: 'Dashboard', component: Dashboard},
+      {path: '/surveys', name: 'Surveys', component: Surveys},
+
+    ]
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: '/auth',
+    redirect: '/login',
+    name: 'Auth',
+    component: AuthLayout,
+    children: [
+      {
+        path: '/login',
+        name: 'Login',
+        component: Login
+      },
+      {
+        path: '/register',
+        name: 'Register',
+        component: Register
+      }
+    ]
   },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.meta.requiresAuth && !store.state.user.token){
+    next({name: 'Login'})
+  } else if (store.state.user.token && (to.name === 'Login' || to.name === 'Register')) {
+    next({name: 'Dashboard'});
+  } else {
+    next()
+  }
 })
 
 export default router;
